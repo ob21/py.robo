2ee71ad113943353043c28a8f37190e7071209ab
opob7414 <olivier.briand@orange.com>
2018-04-26 15:26:22 +0200

    get forms ok and print them

--- robo.py
+++ robo.py
@@ -5,12 +5,14 @@ from robobrowser import RoboBrowser
 from requests import Session
 
 session = Session()
-session.proxies = {'http': 'http://p-goodway.rd.francetelecom.fr:3128'}
-session.auth = ('opob7414', 'belOR@AA')
 
 browser = RoboBrowser(session=session, user_agent='Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101  Firefox/40.1')
-browser.open('http://twitter.com')
+browser.open('http://google.fr')
 
 forms = browser.get_forms()
 
-print(browser.parsed)
+#print(browser.parsed)
+for form in forms:
+ print("****************************************************")
+ print(form.parsed)
+
