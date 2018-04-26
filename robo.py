4d62c0f08391ce02ab5cc796739b2f43edc664c6
OPOB7414 <olivier.briand@orange.com>
2018-04-26 14:31:16 +0200

    update

--- robo.py
+++ robo.py
@@ -1,5 +1,16 @@
 # pip install RoboBrowser
+# http://robobrowser.readthedocs.io/en/latest/
 
 from robobrowser import RoboBrowser
-browser = RoboBrowser()
+from requests import Session
+
+session = Session()
+session.proxies = {'http': 'http://p-goodway.rd.francetelecom.fr:3128'}
+session.auth = ('opob7414', 'belOR@AA')
+
+browser = RoboBrowser(session=session, user_agent='Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101  Firefox/40.1')
 browser.open('http://twitter.com')
+
+forms = browser.get_forms()
+
+print(browser.parsed)
