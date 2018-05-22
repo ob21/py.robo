013238a3d80341b5f9450258817a2230df5826c9
opob7414 <olivier.briand@orange.com>
2018-05-22 23:21:29 +0200

    robo scrapper lbp result

--- robo.py
+++ robo.py
@@ -7,13 +7,16 @@ import re
 
 session = Session()
 
+open = session.get("http://www.leboncoin.fr/")
+print(open.headers)
+
 browser = RoboBrowser(session=session, user_agent='Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101  Firefox/40.1')
 browser.open('http://www.leboncoin.fr/')
 
 links = browser.get_links()
 
-for link in links:
- print(link)
+#for link in links:
+# print(link)
  
 bretagne_tag = browser.find("a", attrs={'id': re.compile("region_5")})
 
@@ -21,32 +24,67 @@ print("****************************************************")
 
 print("bretagne_tag : " + str(bretagne_tag))
 
-bretagne = browser.get_link(bretagne_tag)
+#bretagne = browser.get_link(bretagne_tag)
 
 print("****************************************************")
 
-print(bretagne)
+#print(bretagne)
 
-browser.follow_link(bretagne_tag)
+#browser.follow_link(bretagne_tag)
 
-form = browser.get_form(action="//www.leboncoin.fr/li")
+#print(browser.find("section", attrs={"class":"tabsContent block-white dontSwitch"}))
+#print(browser.find("h1", attrs={"class":"grey small no-border"}))
+
+#browser = RoboBrowser(history=False)
+#browser.open("https://www.leboncoin.fr/annonces/offres/bretagne/")
+
+#search_input = browser.find("input", attrs={'id': "searchbutton"})
+#print("search_input : " + str(search_input))
+
+#form = browser.get_form(action="//www.leboncoin.fr/li")
+
+#form = get_forms()[0]
 
 #print(browser.parsed)
 #for form in forms:
 #print("****************************************************")
 
-#print(form)
+#print("forms keys : " + str(form.keys))
 
-form['q'].value="tablette samsung" 
+#form["q"].value="aspirateur" 
 
-browser.submit_form(form)
+#print("\n")
+#print("form : "+str(form))
+#print("\n")
+
+#print("avant submit_form")
+#print("first item : " + str(browser.find("h2", attrs={"class":"item_title"})))
+#print("rechercher autour de moi : " + str(browser.find("button", attrs={"id":"geolocMain"})))
+
+#browser.submit_form(form)
 
 #print(browser.select("body"))
+#print("après submit_form")
+#print("first item : " + str(browser.find("h2", attrs={"class":"item_title"})))
+#print("rechercher autour de moi : " + str(browser.find("button", attrs={"id":"geolocMain"})))
 
-print("****************************************************")
+#print("****************************************************")
 
-item_infos = browser.select("item_infos")
+browser.open("https://www.leboncoin.fr/annonces/offres/bretagne/?th=1&q=aspirateur&it=1")
 
-print("item_infos : " + str(item_infos))
+#print("first item : " + str(browser.find("h2", attrs={"class":"item_title"})))
+
+results = browser.find_all("section", attrs={"class":"item_infos"})
+
+#print(str(results))
+
+for result in results:
+ print("---")
+ print(result)
+ print("-")
+ print(result.findAll("p")[0].string)
+ print("-")
+ print(result.findAll("p")[1].string)
+
 #print(item_infos[0])
 
