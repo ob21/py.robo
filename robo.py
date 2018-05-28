f2062741fc664af807515cf922cd917f9b3ef15b
opob7414 <olivier.briand@orange.com>
2018-05-28 16:27:55 +0200

    use a listbox and place correctly widgets

--- robo.py
+++ robo.py
@@ -2,7 +2,7 @@
 # apt-get install python3-tk
 # http://robobrowser.readthedocs.io/en/latest/
 
-# pyinstaller robo.py
+# pyinstaller --onefile --windowed robo.py
 
 import tkinter
 from robobrowser import RoboBrowser
@@ -27,18 +27,25 @@ class simpleapp_tk(tkinter.Tk):
 
         button = tkinter.Button(self,text=u"Chercher !",
                                 command=self.OnButtonClick)
-        button.grid(column=1,row=0)
+        button.grid(column=1,row=0, columnspan=2)
 
-        self.labelVariable = tkinter.StringVar()
-        label = tkinter.Label(self,textvariable=self.labelVariable,
-                              anchor="w",justify="left", fg="white",bg="black")
-        label.grid(column=0,row=1,columnspan=2,sticky='EW')
-        self.labelVariable.set(u"Resultats...")
+        #self.labelVariable = tkinter.StringVar()        #label = tkinter.Label(self,textvariable=self.labelVariable,
+        #                      anchor="w",justify="left", fg="white",bg="black")
+        #label.grid(column=0,row=1,columnspan=2,sticky='EW')
+        #self.labelVariable.set(u"Resultats (label)...")
+        
+        self.listVariable = tkinter.StringVar();
+        scrollbar = tkinter.Scrollbar(self, orient="vertical")        
+        scrollbar.grid(row=2, column=2, sticky="NS")
+        listbox = tkinter.Listbox(self, fg="white",bg="black", height=24, listvariable = self.listVariable, yscrollcommand=scrollbar.set)
+        listbox.grid(row=2, column=0, columnspan=2, sticky='NESW')
+        scrollbar["command"] = listbox.yview
 
         self.grid_columnconfigure(0,weight=1)
-        self.resizable(True,True)
+        self.resizable(True,False)
         self.update()
-        self.geometry(self.geometry())       
+        #self.geometry(self.geometry())       
+        self.geometry("600x400") 
         self.entry.focus_set()
         self.entry.selection_range(0, tkinter.END)
         
@@ -60,20 +67,46 @@ class simpleapp_tk(tkinter.Tk):
         price = result.h3.text.strip()
        location = str(result.findAll("p", attrs={"itemprop":"availableAtOrFrom"})[0].text.strip().replace(" ","").replace("\n",""))
        date = result.aside.p.text.strip()
-       search_result = search_result + "\n\n" + title+"   "+price+"   "+location+"    "+date
+       new_result = title+"   "+price+"   "+location+"    "+date
+       search_result = search_result + "\n\n" + new_result
      print("search done")
+     return search_result
+     
+    def SearchLbc2(self, q):
+     print("search : "+q)
+     session = Session()
+     browser = RoboBrowser(session=session, user_agent='Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101  Firefox/40.1')
+     browser.open("https://www.leboncoin.fr/annonces/offres/bretagne/ille_et_vilaine?th=1&q="+q+"&it=1&f=p")
+     results = browser.find_all("section", attrs={"class":"item_infos"})
+     i=0
+     search_result=[]
+     for result in results:
+      if result.find("h2") != None:
+       print("-----------"+str(result))
+       i=i+1
+       title = result.h2.text.strip()
+       price=""
+       if result.find("h3") != None: 
+        price = result.h3.text.strip()
+       location = str(result.findAll("p", attrs={"itemprop":"availableAtOrFrom"})[0].text.strip().replace(" ","").replace("\n",""))
+       date = result.aside.p.text.strip()
+       new_result = str(i) + " - " + title+" - "+price+" - "+location+" - "+date
+       search_result.append(new_result)
+     print("search done : "+str(i))
      return search_result
      
     def OnButtonClick(self):
         #self.labelVariable.set( self.entryVariable.get()+" (You clicked the button)" )
         print("onclick text = "+self.entryVariable.get())
-        self.labelVariable.set( self.SearchLbc(str(self.entryVariable.get())) )
+        #self.labelVariable.set( self.SearchLbc(str(self.entryVariable.get())) )
+        self.listVariable.set(self.SearchLbc2(str(self.entryVariable.get())))
         self.entry.focus_set()
         self.entry.selection_range(0, tkinter.END)
 
     def OnPressEnter(self,event):
         print("onenter text = "+self.entryVariable.get())
-        self.labelVariable.set( self.SearchLbc(str(self.entryVariable.get())) )
+        #self.labelVariable.set( self.SearchLbc(str(self.entryVariable.get())) )
+        self.listVariable.set(self.SearchLbc2(str(self.entryVariable.get())))
         self.entry.focus_set()
         self.entry.selection_range(0, tkinter.END)
 
