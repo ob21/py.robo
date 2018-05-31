5201ffb51198fdd01d82d322b6f1fc96b2ad3d62
opob7414 <olivier.briand@orange.com>
2018-05-31 16:57:04 +0200

    add analyze field with most occurences

--- robo.py
+++ robo.py
@@ -9,6 +9,7 @@ from robobrowser import RoboBrowser
 from requests import Session
 import re
 import sys
+from collections import Counter
 
 
 class simpleapp_tk(tkinter.Tk):
@@ -30,11 +31,11 @@ class simpleapp_tk(tkinter.Tk):
                                 command=self.OnButtonClick)
         button.grid(column=1,row=0, columnspan=2)
 
-        self.labelVariable = tkinter.StringVar()        
-        label = tkinter.Label(self,textvariable=self.labelVariable,
+        self.labelInfoVariable = tkinter.StringVar()        
+        label = tkinter.Label(self,textvariable=self.labelInfoVariable,
                               anchor="w",justify="left", fg="black",bg="grey")
-        label.grid(column=0,row=2,columnspan=2,sticky='EW')
-        self.labelVariable.set(u"Annonces en Ille et Vilaine")
+        label.grid(column=0,row=2,columnspan=3,sticky='EW')
+        self.labelInfoVariable.set(u"Annonces en Ille et Vilaine")
         
         self.listVariable = tkinter.StringVar();
         scrollbar = tkinter.Scrollbar(self, orient="vertical")        
@@ -42,39 +43,27 @@ class simpleapp_tk(tkinter.Tk):
         listbox = tkinter.Listbox(self, fg="white",bg="black", height=24, listvariable = self.listVariable, yscrollcommand=scrollbar.set)
         listbox.grid(row=3, column=0, columnspan=2, sticky='NESW')
         scrollbar["command"] = listbox.yview
+        
+        self.labelTitleResultVariable = tkinter.StringVar()        
+        label = tkinter.Label(self,textvariable=self.labelTitleResultVariable,
+                              anchor="w",justify="left", fg="black",bg="grey")
+        label.grid(column=0,row=4,columnspan=3,sticky='EW')
+        self.labelTitleResultVariable.set(u"Mots courants")
+        
+        self.labelResultVariable = tkinter.StringVar()        
+        label = tkinter.Label(self,textvariable=self.labelResultVariable,
+                              anchor="w",justify="left", fg="black")
+        label.grid(column=0,row=5,columnspan=3,sticky='EW')
+        self.labelResultVariable.set(u"...")
 
         self.grid_columnconfigure(0,weight=1)
         self.resizable(True,False)
-        self.update()
-        #self.geometry(self.geometry())       
-        self.geometry("1000x400") 
+        self.update()      
+        self.geometry("1000x500") 
         self.entry.focus_set()
-        self.entry.selection_range(0, tkinter.END)
-        
-    def SearchLbc(self, q):
-     print("search : "+q)
-     session = Session()
-     browser = RoboBrowser(session=session, user_agent='Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101  Firefox/40.1')
-     browser.open("https://www.leboncoin.fr/annonces/offres/bretagne/ille_et_vilaine?th=1&q="+q+"&it=1&f=p")
-     results = browser.find_all("section", attrs={"class":"item_infos"})
-     i=0
-     search_result=""
-     for result in results:
-      if result.find("h2") != None:
-       print("-----------"+str(result))
-       i=i+1
-       title = result.h2.text.strip()
-       price=""
-       if result.find("h3") != None: 
-        price = result.h3.text.strip()
-       location = str(result.findAll("p", attrs={"itemprop":"availableAtOrFrom"})[0].text.strip().replace(" ","").replace("\n",""))
-       date = result.aside.p.text.strip()
-       new_result = title+"   "+price+"   "+location+"    "+date
-       search_result = search_result + "\n\n" + new_result
-     print("search done")
-     return search_result
+        self.entry.selection_range(0, tkinter.END)       
      
-    def SearchLbc2(self, q):
+    def SearchLbc(self, q):
      print("search : "+q)
      session = Session()
      browser = RoboBrowser(session=session, user_agent='Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101  Firefox/40.1')
@@ -92,39 +81,62 @@ class simpleapp_tk(tkinter.Tk):
         price = result.h3.text.strip()
        location = str(result.findAll("p", attrs={"itemprop":"availableAtOrFrom"})[0].text.strip().replace(" ","").replace("\n",""))
        date = result.aside.p.text.strip()
-       new_result = str(i) + " - " + title+" - "+price+" - "+location+" - "+date
+       new_result = str(i) + " -|- " + title+" -|- "+price+" -|- "+location+" -|- "+date
        search_result.append(new_result)
      print("search done : "+str(i))
      return search_result
      
+    def FindWordsCounts(self, results):
+        words = []
+        print("findSecondWord")
+        for result in results:
+         title = result.split(" -|- ")[1]
+         print(result.split(" -|- ")[1])
+         words.extend(title.split(" "))
+        words.remove("-")
+        words.remove("de")
+        words.remove(str(self.entryVariable.get()))
+        counts = Counter(words)        
+        print("counts" + str(counts))
+        return counts
+     
     def OnButtonClick(self):
-        #self.labelVariable.set( self.entryVariable.get()+" (You clicked the button)" )
         print("onclick text = "+self.entryVariable.get())
-        #self.labelVariable.set( self.SearchLbc(str(self.entryVariable.get())) )
         try:
             print("try to get results")
-            results = self.SearchLbc2(str(self.entryVariable.get()))
+            results = self.SearchLbc(str(self.entryVariable.get()))
+            if not results:
+             results = "aucun resultat"
+            else:
+             for result in results:
+              print(result)
         except:
             print("except")
             results = "error : " + str(sys.exc_info()[0])
         finally:
             print("finally")
             self.listVariable.set(results)
+            self.labelResultVariable.set("done")
             self.entry.focus_set()
             self.entry.selection_range(0, tkinter.END)
 
     def OnPressEnter(self,event):
         print("onenter text = "+self.entryVariable.get())
-        #self.labelVariable.set( self.SearchLbc(str(self.entryVariable.get())) )
+        analyze = "done"
         try:
             print("try to get results")
-            results = self.SearchLbc2(str(self.entryVariable.get()))
+            results = self.SearchLbc(str(self.entryVariable.get()))
+            if not results:
+             results = "aucun resultat"
+            else:
+             analyze = str(self.FindWordsCounts(results).most_common(8))
         except:
             print("except")
             results = "error : "+ str(sys.exc_info()[0])
         finally:
             print("finally")
             self.listVariable.set(results)
+            self.labelResultVariable.set(analyze)            
             self.entry.focus_set()
             self.entry.selection_range(0, tkinter.END)
 
